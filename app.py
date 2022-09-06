6af88119e48903cdb7d7128bbe8646eb1c528197
sharonlaker <51440789+sharonlaker@users.noreply.github.com>
2022-09-06 10:52:56 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,43 @@
+from flask import Flask,request
+from flask_restful import Resource, Api
 import pickle
-import streamlit as st
+import pandas as pd
+from flask_cors import CORS
 
-model = pickle.load(open('/home/sharon/Documents/Desktop/final year project folder/model/sarima_original.pkl', 'rb'))
 
-def main():
-    st.title('water prediction')
+app = Flask(__name__)
+#
+CORS(app)
+# creating an API object
+api = Api(app)
 
-#input variable
-Timestamp = st.text_input('Timestamp')
+#prediction api call
+class prediction(Resource):
+    def get(self,prediction_duration):
+        #prediction_duration = request.args.get('prediction duration')
+        print(prediction_duration)
+        # Let's load the package
+        prediction_duration = [int(prediction_duration)]
+        df = pd.DataFrame(prediction_duration, columns=['Timestamp'])
+        model = pickle.load(open('sarima.pkl', 'rb'))
+        prediction = model.predict(df)
+        prediction = int(prediction[0])
+        return str(prediction)
 
-#prediction code
-if st.button('predict'):
-   makeprediction = model.forecast([[Timestamp]])
-   output = round(makeprediction[0],4)
-   st.success('your water prediction is {}'.format(output))
-   
-   
-if __name__=='__main__':
-    main()
+#data api
+class getData(Resource):
+    def get(self):
+            df1 = pd.read_excel('fake3_1.xlsx')
+            # df =  df.rename({'Timestamp': 'prediction_duration', 'Actual Sales': 'sale'}, axis=1)  # rename columns
+            #print(df.head())
+            #out = {'key':str}
+            res = df1.to_json(orient='records')
+            #print( res)
+            return res
+
+#
+api.add_resource(getData, '/api')
+api.add_resource(prediction, '/prediction/<int:budget>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
